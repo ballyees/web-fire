85f5e31685db6ddb2bcbea5378939795d198b36e
ballyees <kittinan_prompao@elearning.cmu.ac.th>
2020-03-24 23:33:43 +0700

    Updates

---
+++ precache-manifest.873dab234902d082e4053f6b8a97f4d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad6bd3bd7854af7c86134edfd7ff4e9",
+    "url": "/web-fire/index.html"
+  },
+  {
+    "revision": "802c4c36587ff08af96a",
+    "url": "/web-fire/static/css/2.62ebf70e.chunk.css"
+  },
+  {
+    "revision": "32721a85b0f92edd18ed",
+    "url": "/web-fire/static/css/main.0b974771.chunk.css"
+  },
+  {
+    "revision": "802c4c36587ff08af96a",
+    "url": "/web-fire/static/js/2.d30c8d7b.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/web-fire/static/js/2.d30c8d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32721a85b0f92edd18ed",
+    "url": "/web-fire/static/js/main.e3f2dbd4.chunk.js"
+  },
+  {
+    "revision": "3127a5d2510bdd44dc0c",
+    "url": "/web-fire/static/js/runtime-main.d38cf432.js"
+  },
+  {
+    "revision": "cb83a55ca9b912aa11d18153b5d58758",
+    "url": "/web-fire/static/media/dev.cb83a55c.jfif"
+  }
+]);
